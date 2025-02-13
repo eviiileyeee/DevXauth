96019e6abfa49f36ebafd4ca8da6e84a344761bc
harendra <jeaturajabundela@gmail.com>
2025-02-13 18:54:34 +0530

    home pageeeeeeeeee

---
+++ client/src/pages/Hero.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+
+
+const Hero = () => {
+  
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">Welcome to DevXauth</h1>
+        <p className="text-lg mb-8">
+          DevXauth is a platform for developers to connect, collaborate, and learn. Join us today!
+          </p>
+        </div>
+    </>
+  );
+};
+
+export default Hero;
